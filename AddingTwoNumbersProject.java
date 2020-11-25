7894ad8048d461c233af46ef137c054621ae8f64
Carol Prado <72031358+Carol-Prado@users.noreply.github.com>
2020-11-25 11:25:44 +0000

    Add files via upload

---
+++ AddingTwoNumbersProject.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package addingtwonumbersproject;
+
+/**
+ *
+ * @author ca_ro
+ */
+public class AddingTwoNumbersProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int firstNumber = 40;
+        System.out.println(firstNumber);
+        int secondNumber = 63;
+        System.out.println(secondNumber);
+                
+    }
+    
+}
